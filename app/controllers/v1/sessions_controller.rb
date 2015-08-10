bd5f4818f5ccb748763d0ed370405b772d908ae6
Brandon <brandon.kirchner@blackbaud.com>
2015-08-10 10:46:26 -0500

    username isnt the same as email.

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -3,7 +3,7 @@ module V1
     skip_before_action :authenticate_user_from_token!
 
     def create
-      @user = User.find_for_database_authentication(email: params[:username])
+      @user = User.find_for_database_authentication(username: params[:username])
       return invalid_login_attempt unless @user
 
       if @user.valid_password? params[:password]
